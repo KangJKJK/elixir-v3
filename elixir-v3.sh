c1f602f8c91c3c9c4a3138d95a72b17a08c9933f
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-09 09:04:59 +0900

    Update elixir-v3.sh

--- elixir-v3.sh
+++ elixir-v3.sh
@@ -58,48 +58,10 @@ else
     echo -e "${GREEN}Docker가 이미 설치되어 있습니다.${NC}"
 fi
 
-echo -e "${BOLD}${CYAN}검증자 지갑 생성 중...${NC}"
-
-read -p "기존 지갑의 니모닉 구문을 입력하세요: " MNEMONIC_INPUT
-
-cat << EOF > use_existing_wallet.js
-const { Wallet } = require('ethers');
-const fs = require('fs');
-
-// 니모닉 구문으로 지갑 생성
-try {
-    const wallet = Wallet.fromMnemonic('$MNEMONIC_INPUT');
-
-    const mnemonic = wallet.mnemonic.phrase;
-    const address = wallet.address;
-    const privateKey = wallet.privateKey;
-
-    const walletData = \`
-Mnemonic: \${mnemonic}
-Address: \${address}
-Private Key: \${privateKey}
-\`;
-
-    const filePath = 'validator_wallet.txt';
-    fs.writeFileSync(filePath, walletData);
-
-    console.log('');
-    console.log('검증자 지갑 니모닉 구문:', mnemonic);
-    console.log('검증자 지갑 주소:', address);
-    console.log('검증자 지갑 개인 키:', privateKey);
-    console.log('\\x1B[32m지갑 자격 증명이 \\x1b[35m validator_wallet.txt\\x1B[0m에 저장되었습니다.');
-} catch (error) {
-    console.error('유효하지 않은 니모닉 구문입니다. 다시 시도하세요.');
-    process.exit(1);
-}
-EOF
-
-node use_existing_wallet.js
-echo ""
-
 ENV_FILE="validator.env"
 
 echo -e "${BOLD}${CYAN}${ENV_FILE} 파일 생성 중...${NC}"
+read -p "검증자 지갑의 프라이빗 키를 입력하세요: " PRIVATE_KEY
 echo "ENV=testnet-3" > $ENV_FILE
 IP_ADDRESS=$(curl -s ifconfig.me)
 echo "STRATEGY_EXECUTOR_IP_ADDRESS=$IP_ADDRESS" >> $ENV_FILE
@@ -124,7 +86,7 @@ echo -e "${BOLD}${YELLOW}1. 해당 주소로 이동하세요: https://testnet-3.
 echo -e "${BOLD}${YELLOW}2. Sepolia Ethereum이 있는 지갑을 연결하세요 (이 지갑은 검증자 지갑 주소가 아니어야 합니다).${NC}"
 echo -e "${BOLD}${YELLOW}3. Sepolia에서 MOCK Elixir 토큰을 발행하세요${NC}"
 echo -e "${BOLD}${YELLOW}4. MOCK 토큰을 스테이킹하세요${NC}"
-echo -e "${BOLD}${YELLOW}5. 이제 커스텀 검증자를 클릭하고 검증자 지갑 주소: $VALIDATOR_ADDRESS를 입력하세요.${NC}"
+echo -e "${BOLD}${YELLOW}5. 이제 커스텀 검증자를 클릭하고 검증자 지갑 주소를 입력하세요.(아까 입력한 개인키의 지갑주소)${NC}"
 echo ""
 
 read -p "위 단계를 완료하셨나요? (y/n): " response
@@ -153,4 +115,3 @@ echo ""
 
 echo -e "${GREEN}모든 작업이 완료되었습니다. 컨트롤+A+D로 스크린을 종료해주세요.${NC}"
 echo -e "${GREEN}스크립트 작성자: https://t.me/kjkresearch${NC}"
-
