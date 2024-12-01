8baae15355fc82afdea9b06dd86f6cbc64f34f63
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-12-01 17:37:29 +0900

    Update elixir-v3.sh

--- elixir-v3.sh
+++ elixir-v3.sh
@@ -139,7 +139,7 @@ case "$choice" in
     echo -e "${GREEN}모든 사용 중인 포트가 허용되었습니다.${NC}"
 
     echo -e "${BOLD}${CYAN}Docker 실행 중...${NC}"
-    docker run -d --env-file validator.env --name elixir -p 17690:17690 --restart unless-stopped elixirprotocol/validator:testnet
+    docker run -d --env-file validator.env --name elixir -p 17691:17690 --restart unless-stopped elixirprotocol/validator:testnet
     echo ""
 
     # 현재 사용 중인 포트 확인 및 허용
